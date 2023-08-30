b4fb22a4dba61ee4bc11b03cbb0cca2bba895063
miftahzulfikar <miftah.zulfikar@tokopedia.com>
2023-08-30 15:29:08 +0700

    connect sheets api

--- src/app.js
+++ src/app.js
@@ -1,27 +1,39 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const { google } = require("googleapis");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require("./middlewares");
+
+const sheets = google.sheets({
+  version: "v4",
+  auth: "AIzaSyAN77FP5EQeqWt5kw5bQXw5XNDmJkDpoVg",
+});
+
+const params = {
+  spreadsheetId: "1XTDu_6JuLsLoXy2OcqnjUDCCHTOzZDxxqPadxrWRfyk",
+  range: "Action!A2:F",
+};
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+app.get("/", async (req, res) => {
+  const data = await sheets.spreadsheets.values.get(params, (err, result) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+    res.json(result.data.values);
   });
 });
-
-app.use('/api/v1', api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
